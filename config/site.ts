7cbcb0fe4ecf62158809945b1984dc8f7e0915d6
dtdev <54771496+dtdev-hub@users.noreply.github.com>
2026-07-20 16:29:33 +0700

    Initial commit: Setup personal portfolio project

---
+++ config/site.ts
@@ -0,0 +1,41 @@
+export const siteConfig = {
+  name: 'aadi.is-dev',
+  url: 'https://aadi.is-a.dev',
+  description: 'Portfolio of Aditya Domle, Full-Stack Web Developer & DevOps enthusiast.',
+  keywords: [
+    'Aditya Domle',
+    'aditya domle',
+    'aditya.com',
+    'Aditya Domle Portfolio',
+    'Full Stack Developer',
+    'Web Developer',
+    'DevOps Engineer',
+    'Next.js Developer',
+    'React Developer',
+    'Node.js Developer',
+    'Aditya Domle Projects',
+    'Aditya Domle Skills',
+    'Aditya Domle Experience',
+    'Aditya Domle Education',
+    'Aditya Domle Contact',
+    'Aditya Domle Resume',
+  ],
+  links: {
+    shadcn: 'https://ui.shadcn.com/',
+    telegram: 'https://t.me/nexjsx',
+    twitter: 'https://x.com/env_aditya',
+    github: 'https://github.com/adityadomle',
+    githubProfile: 'https://github.com/adityadomle',
+    linkedin: 'https://www.linkedin.com/in/adityadomle/',
+    resume: 'https://drive.google.com/file/d/your-resume-id/view', // Add your resume link here
+    email: 'mailto:adityadomle14@gmail.com',
+    githubUsername: 'adityadomle',
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
+
+export const META_THEME_COLORS = {
+  light: '#ffffff',
+  dark: '#09090b',
+};
